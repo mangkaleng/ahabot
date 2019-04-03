adfd861120e9cc2404d1c129fd5d1508815d7943
mangkaleng <45580853+mangkaleng@users.noreply.github.com>
2019-04-03 14:15:50 +0700

    Update run.php

--- run.php
+++ run.php
@@ -8,7 +8,7 @@
 date_default_timezone_set("Asia/Jakarta");
 class bigtoken extends modules
 {
-        private $domains = "@sharklasers.com";
+        private $domains = "@spam4.me";
 
         public function registerAccount($referralCode)
         {
